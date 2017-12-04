3cd6a32e236273e6509d47138654fce783bb1220
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-12-04 19:00:36 +0100

    Add responsive navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
   def homepage
     render layout: 'homepage'
   end
+
+  def project
+    render layout: 'homepage'
+  end
+
+  def team
+    render layout: 'homepage'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   root to:"pages#homepage"
 
+  get 'project', to:"pages#project"
+  get 'team', to:"pages#team"
+
   resources :products
 end
