6ac2e8f93c8114b857f0d5d037f269218d5e8fc7
Antoine Guilguet <guilguet.antoine@gmail.com>
2018-02-12 22:40:52 +0100

    Reroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  root to:"pages#homepage"
-
+  root to:"pages#welcome"
+  
+  get 'homepage', to:"pages#homepage"
   get 'welcome', to:"pages#landing_page"
 
   post 'save_sourcing', to:"pages#save_sourcing"
