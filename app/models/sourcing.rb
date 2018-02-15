acacfae1f3dfd76c7120a5b98f9c7e73243a0734
Antoine Guilguet <guilguet.antoine@gmail.com>
2018-02-15 15:06:08 +0100

    Add sendgrid integration and notification mailer

--- app/models/sourcing.rb
+++ app/models/sourcing.rb
@@ -2,4 +2,12 @@ class Sourcing < ApplicationRecord
   validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
   validates :produit, presence: true
   validates :exigence, presence: true
+
+  after_create :send_sourcing_notification
+
+  private
+
+  def send_sourcing_notification
+    SourcingMailer.sourcing_request(self).deliver_now
+  end
 end
